b1bd621741fd8d392d37434e5e4c36bddb9ec4b2
rywaroy <464485682@qq.com>
2020-10-13 14:22:13 +0800

    feat: 页码

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,9 @@ export default class App extends React.Component {
     this.state = {
       list: [],
       formData: {},
+      current: 1,
+      total: 0,
+      pageSize: 10,
     };
   }
 
@@ -17,38 +20,48 @@ export default class App extends React.Component {
    * 搜索
    */
   onSearch = values => {
-    this.setState(
-      {
+    this.setState({
         formData: values,
-      },
-      () => {
-        this.getList();
-      },
-    );
+      }, () => this.getList());
   };
 
   /**
    * 重置
    */
   onReset = () => {
-    this.setState(
-      {
+    this.setState({
         formData: {},
-      },
-      () => {
-        this.getList();
-      },
-    );
+      }, () => this.getList());
   };
 
-  getList() {
-    const { formData } = this.state;
-    getStudentList(formData).then(res => {
+  getList(params) {
+    const { formData, current, pageSize } = this.state;
+    getStudentList({
+      ...formData,
+      current,
+      pageSize,
+    }).then(res => {
       this.setState({
         list: res.data,
+        total: res.total,
       });
     });
   }
+
+  // 分页
+  onPageChange = (current) => {
+    this.setState({
+      current,
+    }, () => this.getList());
+  };
+
+  // 页码变化
+  onShowSizeChange = (current, size) => {
+    this.setState({
+      current,
+      pageSize: size,
+    }, () => this.getList());
+  };
 
   componentDidMount() {
     this.getList();
@@ -61,12 +74,24 @@ export default class App extends React.Component {
       { title: '性别', dataIndex: 'gender' },
       { title: '操作', key: 'action', render: () => <a>编辑</a> },
     ];
-    const { list } = this.state;
+    const { list, total, current, pageSize } = this.state;
+    const pagination = {
+      pageSizeOptions: [ '10', '30', '50' ],
+      showSizeChanger: true,
+      showQuickJumper: true,
+      showTotal: (total, range) => `共 ${total} 条`,
+      total,
+      current,
+      pageSize,
+      onChange: this.onPageChange,
+      onShowSizeChange: this.onShowSizeChange
+    };
+    
 
     return (
       <div className={styles.wrap}>
         <ListFilter onSearch={this.onSearch} onReset={this.onReset} />
-        <Table columns={columns} dataSource={list} rowKey="id" />
+        <Table columns={columns} dataSource={list} rowKey="id" pagination={pagination} />
       </div>
     );
   }
--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,12 @@
 import Mock from 'mockjs';
 
 export function getStudentList(params = {}) {
-  const { name, age, gender } = params;
+  const { name, age, gender, current = 1, pageSize = 10 } = params;
+  console.log(`发起请求，页码：${current}`);
   const mockData = Mock.mock({
     status: 200,
-    'data|3-6': [
+    total: 50,
+    [`data|${pageSize}`]: [
       {
         name: name ? name : '@cname',
         age: age ? age : '@integer(10, 20)',
