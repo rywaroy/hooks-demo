6026b1ed5e09a3cc7f7195d96d962b71d9f5502b
rywaroy <464485682@qq.com>
2020-10-16 14:36:08 +0800

    feat: 函数式组件

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Table, Button, message } from 'antd';
 import ListFilter from '../components/ListFilter';
 import UserModal from '../components/UserModal';
@@ -7,178 +7,95 @@ import BTable from '../components/BTable';
 import aHOC from '../hoc/aHOC';
 import bHOC from '../hoc/bHOC';
 import { getStudentList, addStudent, updateStudent } from '../services';
+import { useAntdTable } from 'behooks';
 import styles from './index.less';
 
-const ATable = aHOC(BTable);
+// const ATable = aHOC(BTable);
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      formData: {},
-      current: 1,
-      total: 0,
-      pageSize: 10,
-      visible: false,
-      key: 1,
-      userInfo: {},
-    };
-  }
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const [key, setKey] = useState(1);
+  const [userInfo, setUserInfo] = useState({});
 
-  /**
-   * 搜索
-   */
-  onSearch = values => {
-    this.getList({
-      formData: values,
-      current: 1,
-    });
-  };
+  const formRef = useRef(null);
 
-  /**
-   * 重置
-   */
-  onReset = () => {
-    this.getList({
-      formData: {},
-      current: 1,
-    });
-  };
-
-  getList = (params = {}) => {
-    this.setState(params, () => this.fetch());
-  }
-
-  /**
-   * 请求列表
-   */
-  fetch() {
-    const { formData, current, pageSize } = this.state;
-    getStudentList({
+  const getData = ({ current, pageSize }, formData) => {
+    return getStudentList({
       ...formData,
       current,
       pageSize,
-    }).then(res => {
-      this.setState({
-        list: res.data,
-        total: res.total,
-      });
     });
-  }
+  };
+
+  const { tableProps, refresh, search } = useAntdTable(getData, {
+    form: formRef.current ? formRef.current.getForm() : false,
+    formatResult: (res) => ({
+      list: res.data,
+      total: res.total,
+    }),
+  });
+
+  const { submit, reset } = search;
 
   /**
    * 打开编辑学生弹窗
    */
-  openUserModal = record => {
-    this.setState({
-      userInfo: record,
-      visible: true,
-      key: Math.random(),
-    })
+  const openUserModal = record => {
+    setVisible(true);
+    setKey(Math.random());
+    setUserInfo(record);
   }
 
   /**
    * 关闭编辑学生弹窗
    */
-  closeUserModal = () => {
-    this.setState({
-      visible: false,
-    })
+  const closeUserModal = () => {
+    setVisible(false);
   }
 
   /**
    * 编辑/新增
    */
-  edit = values => {
-    const { userInfo } = this.state;
+  const edit = values => {
     if (userInfo.id) { // 编辑
       updateStudent({
         id: userInfo.id,
         ...values,
       }).then(() => {
-        this.getList();
-        this.closeUserModal();
+        refresh();
+        closeUserModal();
         message.success('编辑成功');
       });
     } else { // 新增
       addStudent(values)
         .then(() => {
-          this.getList();
-          this.closeUserModal();
+          refresh();
+          closeUserModal();
           message.success('新增成功');
         });
     }
   }
 
-  componentDidMount() {
-    this.getList();
-  }
+  const columns = [
+    { title: '姓名', dataIndex: 'name' },
+    { title: '年龄', dataIndex: 'age' },
+    { title: '性别', dataIndex: 'gender' },
+    { title: '操作', key: 'action', render: (record) => <a onClick={() => openUserModal(record)}>编辑</a> },
+  ];
 
-  render() {
-    const columns = [
-      { title: '姓名', dataIndex: 'name' },
-      { title: '年龄', dataIndex: 'age' },
-      { title: '性别', dataIndex: 'gender' },
-      { title: '操作', key: 'action', render: (record) => <a onClick={() => this.openUserModal(record)}>编辑</a> },
-    ];
-    const { list, total, current, pageSize, visible, key, userInfo } = this.state;
-
-    const pagination = getPagination(this.getList, total, current, pageSize);
-
-    return (
-      <div className={styles.wrap}>
-        <ListFilter onSearch={this.onSearch} onReset={this.onReset} />
-        <Button type="primary" onClick={() => this.openUserModal({})}>新增</Button>
-        <Table columns={columns} dataSource={list} rowKey="id" pagination={pagination} />
-        {/* <ATable
-          total={total}
-          current={current}
-          pageSize={pageSize}
-          getList={this.getList}
-          columns={columns}
-          dataSource={list}
-          rowKey="id" /> */}
-        <UserModal
-          visible={visible}
-          key={key}
-          userInfo={userInfo}
-          onCancel={this.closeUserModal}
-          onOk={this.edit} />
-      </div>
-    );
-  }
+  return (
+    <div className={styles.wrap}>
+      <ListFilter onSearch={submit} onReset={reset} ref={formRef} />
+      <Button type="primary" onClick={() => openUserModal({})}>新增</Button>
+      <Table columns={columns} rowKey="id" {...tableProps} />
+      <UserModal
+        visible={visible}
+        key={key}
+        userInfo={userInfo}
+        onCancel={closeUserModal}
+        onOk={edit} />
+    </div>
+  );
 }
 
-function getPagination(getList, total, current, pageSize) {
-  /**
-   * 分页
-   */
-  const onPageChange = (current) => {
-    getList({
-      current,
-    });
-  };
-
-  /**
-   * 页码变化
-   */
-  const onShowSizeChange = (current, pageSize) => {
-    getList({
-      current,
-      pageSize,
-    });
-  };
-
-  return {
-    pageSizeOptions: [ '10', '30', '50' ],
-    showSizeChanger: true,
-    showQuickJumper: true,
-    showTotal: (total, range) => `共 ${total} 条`,
-    total,
-    current,
-    pageSize,
-    onChange: onPageChange,
-    onShowSizeChange: onShowSizeChange
-  };
-}
+export default App;
